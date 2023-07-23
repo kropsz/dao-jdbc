2ddf77804d78c7ddf25c8426441c0acbaa711942
Pedro Henrique Guedes <kropinho211@outlook.com>
2023-07-23 17:34:15 -0300

    "Dao classes"

---
+++ src/model/dao/DaoFactory.java
@@ -0,0 +1,10 @@
+package model.dao;
+
+import model.dao.impl.SellerDaoJDBC;
+
+public class DaoFactory {
+    private static SellerDao createSellerDao(){
+        return new SellerDaoJDBC();
+    }
+
+}
