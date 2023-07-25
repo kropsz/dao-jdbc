8c8a6bf8761f5657c66cad858ccfe92ee75fd7e2
Pedro Henrique Guedes <kropinho211@outlook.com>
2023-07-25 11:22:35 -0300

    "delete implementation"

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,7 @@ package application;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
@@ -10,6 +11,8 @@ import model.entities.Seller;
 
 public class Program {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println("===TEST 1===");
@@ -44,6 +47,14 @@ public class Program {
         sellerDao.update(seller);
         System.out.println("Update completed !");
 
+        System.out.println("===TEST 6===");
+        System.out.println("Enter id for delete: ");
+        int id = sc.nextInt();
+        sellerDao.delete(id);
+        System.out.println("Delete completed");
 
+
+
+        sc.close();
     }
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -86,8 +86,21 @@ public class SellerDaoJDBC implements SellerDao {
 
     @Override
     public void delete(Integer id) {
-
-    }
+            PreparedStatement st = null;
+            try {
+                st = conn.prepareStatement("DELETE FROM seller WHERE Id = ?");
+                st.setInt(1, id);
+                int rows = st.executeUpdate();
+                if(rows == 0){
+                    throw new DbException("Invalid Id!");
+                }
+            } catch (SQLException e) {
+                throw new DbException(e.getMessage());
+            }
+            finally{
+                DB.closeStatement(st);
+            }
+    }       
 
     @Override
     public Seller findById(Integer id) {
